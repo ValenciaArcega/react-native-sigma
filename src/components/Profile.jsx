9b01f553a34a1cda9a3011d5c712caf8fa437133
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2023-06-26 19:17:43 -0600

    fixes: new input fields and checking for input number entries

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,52 +1,55 @@
-import { s } from "../styles/st-profile";
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { NavBar } from "./NavBar";
-import { KeyboardAvoidingView, ScrollView, TextInput, View, Text, TouchableOpacity, Alert } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { getAuth } from "firebase/auth";
-import { useEffect, useState } from "react";
-import { db, firebaseApp } from '../credentials';
-import { collection, doc, setDoc } from "firebase/firestore";
-import { usuarios } from "../data/data-test";
+import { s } from "../styles/st-profile"
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import { NavBar } from "./NavBar"
+import { KeyboardAvoidingView, ScrollView, TextInput, View, Text, TouchableOpacity, Alert } from "react-native"
+import { useNavigation } from "@react-navigation/native"
+import { getAuth } from "firebase/auth"
+import { useEffect, useState } from "react"
+import { db, firebaseApp } from '../credentials'
+import { collection, doc, setDoc } from "firebase/firestore"
+import { usuarios } from "../data/data-test"
 
 export function Profile() {
-  Icon.loadFont();
-  const auth = getAuth(firebaseApp);
-  const navigation = useNavigation();
-  /*
+  Icon.loadFont()
+  const auth = getAuth(firebaseApp)
+  const navigation = useNavigation()
+
   const [car, setCar] = useState({
     nombre: '',
-    apellidos: '',
+    numero: '',
     marca: '',
     modelo: '',
-  });
-  */
+    año: '',
+  })
 
   function signOutUser() {
     auth.signOut()
       .then(() => navigation.replace('Sign'))
-      .catch(err => Alert.alert(err.message));
+      .catch(err => Alert.alert(err.message))
   };
 
-  const handleChangeText = (name, value) => setCar({ ...car, [name]: value });
-  /*
-    const registerCarData = async () => {
-      try {
-        const user = auth.currentUser;
-        const docRef = doc(collection(db, 'users'), user.email);
-        await setDoc(docRef, {
-          nombre: car.nombre,
-          apellidos: car.apellidos,
-          marca: car.marca,
-          modelo: car.modelo,
-        });
-        Alert.alert('Guardado ✅');
-        // console.log("Document written with ID: ", docRef.id);
-      } catch (e) {
-        console.error("Error: ", e);
-      }
-    };
-  */
+  const handleChangeText = (name, value) => setCar({ ...car, [name]: value })
+
+  const registerCarData = async () => {
+    try {
+      const user = auth.currentUser
+      const docRef = doc(collection(db, 'users'), user.email)
+      await setDoc(docRef, {
+        nombre: car.nombre,
+        numero: car.numero,
+        marca: car.marca,
+        modelo: car.modelo,
+        año: car.año,
+      })
+      Alert.alert('Guardado ✅')
+      // console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error: ", e)
+    }
+  }
+
+
+  /* ⛔ To add 100 records
   const registerCarData = async () => {
     try {
       console.log('Guardando registros...');
@@ -66,6 +69,7 @@ export function Profile() {
       console.error("Error: ", e);
     }
   };
+  */
   return (
     <KeyboardAvoidingView behavior="padding" style={{ marginTop: 32, flex: 1 }} >
       <ScrollView>
@@ -75,22 +79,47 @@ export function Profile() {
 
           <View style={s.ContainerInput}>
             <Text style={s.ContainerInputText}>Nombre</Text>
-            <TextInput style={s.Input} placeholder="Pedro Issac" onChangeText={value => handleChangeText("nombre", value)}></TextInput>
+            <TextInput
+              style={s.Input}
+              placeholder="Pedro Issac"
+              type="text"
+              inputMode="text"
+              onChangeText={value => handleChangeText("nombre", value)}></TextInput>
           </View>
 
           <View style={s.ContainerInput}>
-            <Text style={s.ContainerInputText}>Apellidos</Text>
-            <TextInput style={s.Input} placeholder="Carmona Arcega" onChangeText={value => handleChangeText("apellidos", value)}></TextInput>
+            <Text style={s.ContainerInputText}>Numero</Text>
+            <TextInput
+              style={s.Input}
+              placeholder="5540992376"
+              type="number"
+              pattern="[0-9]*"
+              inputMode="numeric"
+              onChangeText={value => handleChangeText("numero", value)}></TextInput>
           </View>
 
           <View style={s.ContainerInput}>
             <Text style={s.ContainerInputText}>Marca del vehiculo</Text>
-            <TextInput style={s.Input} placeholder="Chevrolet" onChangeText={value => handleChangeText("marca", value)}></TextInput>
+            <TextInput
+              style={s.Input}
+              placeholder="Chevrolet"
+              onChangeText={value => handleChangeText("marca", value)}></TextInput>
           </View>
 
           <View style={s.ContainerInput}>
             <Text style={s.ContainerInputText}>Modelo del vehiculo</Text>
-            <TextInput style={s.Input} placeholder="Aveo 2022" onChangeText={value => handleChangeText("modelo", value)}></TextInput>
+            <TextInput style={s.Input} placeholder="Aveo" onChangeText={value => handleChangeText("modelo", value)}></TextInput>
+          </View>
+
+          <View style={s.ContainerInput}>
+            <Text style={s.ContainerInputText}>Año del modelo</Text>
+            <TextInput
+              style={s.Input}
+              placeholder="2023"
+              type="number"
+              pattern="[0-9]*"
+              inputMode="numeric"
+              onChangeText={value => handleChangeText("año", value)}></TextInput>
           </View>
 
           <TouchableOpacity onPress={() => registerCarData()} style={s.ButtonRegisterCar}>
@@ -106,5 +135,5 @@ export function Profile() {
       </ScrollView>
       <NavBar />
     </KeyboardAvoidingView>
-  );
+  )
 };
